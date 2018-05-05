5194a114839c52c319f3571b45c922dd5f12fd10
caiodsc <caio.dscamara@gmail.com>
2018-05-05 08:08:27 -0400

    Notices e notifications

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'home#index'
+  post '/create-notice', to: 'notices#create'
+  get '/notifications', to: 'notices#notifications'
   #get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
